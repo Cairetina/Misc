4901d1c9d324508fc07f71a5b2b642ee2ad7fa0f
Cairetina <AltriaPendron@gmail.com>
2021-02-19 17:56:23 +0800

    Update IP_bili_cn.js

--- IP_bili_cn.js
+++ IP_bili_cn.js
@@ -50,7 +50,7 @@ var obj = JSON.parse(body)["data"];
 var emoji = flags.get(obj['country'])? flags.get(obj['country']):"🏴‍☠️"
 emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
-var title = emoji + " " + City_ValidCheck(obj['province']);
+var title = emoji + "  " + City_ValidCheck(obj['province']);
 var subtitle = ISP_ValidCheck(obj['isp']);
 var ip = obj['addr']; 
 var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
