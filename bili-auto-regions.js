5889f33d8950b0c4a0da2996b017dc38e21a6a43
Cairetina <AltriaPendron@gmail.com>
2022-01-11 22:19:02 +0800

    Update bili-auto-regions.js

--- bili-auto-regions.js
+++ bili-auto-regions.js
@@ -1,24 +1,44 @@
-const raw = JSON.parse($response.body);
-const data = raw.data || raw.result || {};
-
 const Group = 'BiliRegion';
 const CN = 'DIRECT';
 const HK = 'ProxySnell';
 const current = $surge.selectGroupDetails().decisions[Group];
 
-const area = (() => {
-	if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(data.title)) {
-		if (current != HK) return HK;
-	} else if (current != CN) return CN;
-})()
-
-if (area) {
-	const change = $surge.setSelectGroupPolicy(Group, area);
-	if (change) {
-		$done();
+if (typeof($response) !== 'undefined') {
+	const raw = JSON.parse($response.body);
+	const data = raw.data || raw.result || {};
+	const area = (() => {
+		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(data.title)) {
+			if (current != HK) return HK;
+		} else if (current != CN) return CN;
+	})()
+	if (area) {
+		const change = $surge.setSelectGroupPolicy(Group, area);
+		if (change) {
+			$done();
+		} else {
+			$done({});
+		}
 	} else {
 		$done({});
 	}
 } else {
-	$done({});
+	const raw = $request.url;
+	const res = {
+		url: raw.replace(/%20(%E6%B8%AF|%E4%B8%AD)&/g, '&')
+	};
+	const area = (() => {
+		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(raw)) {
+			if (current != HK) return HK;
+		} else if (current != CN) return CN;
+	})()
+	if (area) {
+		const change = $surge.setSelectGroupPolicy(Group, area);
+		if (change) {
+			$done(res);
+		} else {
+			$done({});
+		}
+	} else {
+		$done({});
+	}
 }
