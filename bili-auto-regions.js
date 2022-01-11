546597876031be8b4701952050e3bf83cef30fe1
Cairetina <AltriaPendron@gmail.com>
2022-01-11 16:30:18 +0800

    Update bili-auto-regions.js

--- bili-auto-regions.js
+++ bili-auto-regions.js
@@ -1,21 +1,19 @@
-const Group = $persistentStore.read('BiliArea_Policy') || '哔哩哔哩';
-const CN = $persistentStore.read('BiliArea_CN') || 'DIRECT';
-const TW = $persistentStore.read('BiliArea_TW') || '台湾';
-const HK = $persistentStore.read('BiliArea_HK') || '港台';
+const Group = 'BiliRegion';
+const CN = 'DIRECT';
+const HK = 'ProxySnell';
+const current = $surge.selectGroupDetails().decisions[Group];
 
-var obj = JSON.parse($response.body),
-	obj = (obj.result || obj.data || {}).title || '';
-const current = $surge.selectGroupDetails().decisions[Group] || '策略组配置错误'
-const str = (() => {
-	if (obj.match(/\u50c5[\u4e00-\u9fa5]+\u6e2f/)) {
+const raw = JSON.parse($response.body);
+const data = raw.data || raw.result || {};
+
+const area = (() => {
+	if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(data.title)) {
 		if (current != HK) return HK;
-	} else if (obj.match(/\u50c5[\u4e00-\u9fa5]+\u53f0/)) {
-		if (current != TW) return TW;
 	} else if (current != CN) return CN;
 })()
 
-if (str) {
-	const change = $surge.setSelectGroupPolicy(Group, str);
+if (area) {
+	const change = $surge.setSelectGroupPolicy(Group, area);
 	if (change) {
 		$done();
 	} else {
